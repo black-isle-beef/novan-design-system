52ef8bb281cf31ba2af37ada1e2e696a164ee097
Stuart Denovan <stuart.denovan@gmail.com>
2026-08-30 16:22:35 +0100

    feat: add main entry point for Angular application

--- projects/design-system/src/lib/components/forms/forms.component.spec.ts
+++ projects/design-system/src/lib/components/forms/forms.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DsFormsComponent } from './forms.component';
-import { DsIndeterminateDirective } from './indeterminate.directive';
+import { DsIndeterminateDirective } from './checkboxes/indeterminate.directive';
 import { Component, signal } from '@angular/core';
 
 @Component({
